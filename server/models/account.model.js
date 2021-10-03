ea9785e04f8ff601c8daf4792f1949ae36510ffc
mahadi-devo <mahadihassan083@gmail.com>
2021-10-04 00:14:54 +0530

    issues fix

--- server/models/account.model.js
+++ server/models/account.model.js
@@ -94,7 +94,7 @@ class Account {
         attributes: ["id", "creditAmount", "email", "qrCode"],
         include: {
           model: models.passengers,
-          as: "id_passenger",
+          as: "passenger",
         },
         where: { id: accountId },
       });
